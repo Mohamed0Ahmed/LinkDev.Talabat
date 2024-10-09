06fdb48a1c3fc9db1cc09b3c5193d462b8e34db7
Mohamed Ahmed <mahmmaad96@gmail.com>
2024-10-09 20:17:56 +0300

    Unit Of Work

---
+++ LinkDev.Talabat.Infrastructure.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Infrastructure.Abstractions;
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using System.Collections.Concurrent;
+
+namespace LinkDev.Talabat.Infrastructure.Persistence.Repositories
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+
+
+        #region Services
+
+
+        private readonly StoreContext _dbContext;
+        private readonly ConcurrentDictionary<string, object> _repositories; 
+
+        public UnitOfWork(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+            _repositories = new ConcurrentDictionary<string, object>();
+        } 
+
+
+        #endregion
+
+
+
+
+
+        public IGenericRepository<TEntity, TKey> GetGenericRepository<TEntity, TKey>()
+                                 where TEntity : BaseEntity<TKey>
+                                 where TKey : IEquatable<TKey>
+        {
+         
+
+            return (IGenericRepository<TEntity, TKey>) _repositories.GetOrAdd(typeof(TEntity).Name, new GenericRepository<TEntity , TKey>(_dbContext));
+
+        }
+
+
+
+        public async Task<int> CompleteAsync() => await _dbContext.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync() => await _dbContext.DisposeAsync();   
+
+       
+    }
+}
---
+++ LinkDev.Talabat.Infrastructure/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructure.Abstractions
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IGenericRepository<TEntity, TKey> GetGenericRepository<TEntity, TKey>()
+                           where TEntity : BaseEntity<TKey>
+                           where TKey : IEquatable<TKey>;
+
+
+        Task<int> CompleteAsync();
+    }
+}

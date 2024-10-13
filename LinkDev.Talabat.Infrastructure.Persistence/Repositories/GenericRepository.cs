6ecd3090f1ba8740ea2c686baa91849dc9b44b91
Mohamed Ahmed <mahmmaad96@gmail.com>
2024-10-13 13:43:45 +0300

    Loading Navigational Properties

--- LinkDev.Talabat.Infrastructure.Persistence/Repositories/GenericRepository.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
 using LinkDev.Talabat.Infrastructure.Common.Abstractions;
 using LinkDev.Talabat.Infrastructure.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
@@ -28,17 +29,30 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Repositories
 
 
         public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
-            => withTracking ? await _dbContext.Set<TEntity>().ToListAsync() : await _dbContext.Set<TEntity>().AsNoTrackingWithIdentityResolution().ToListAsync();
+        {
 
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return withTracking ?
+                       (IEnumerable<TEntity>)await _dbContext.Set<Product>().Include(p => p.Brand).Include(p => p.Category).ToListAsync() :
+                       (IEnumerable<TEntity>)await _dbContext.Set<Product>().Include(p => p.Brand).Include(p => p.Category).AsNoTracking().ToListAsync();
+            }
+
+
+            return withTracking ?
+            await _dbContext.Set<TEntity>().ToListAsync() :
+            await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+
+        }
 
 
         public async Task<TEntity?> GetAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
 
 
-        public async Task AddAsync(TEntity entity)=> await _dbContext.AddAsync(entity);
+        public async Task AddAsync(TEntity entity) => await _dbContext.AddAsync(entity);
 
         public void Delete(TEntity entity) => _dbContext.Remove(entity);
 
-        public void Update(TEntity entity)=> _dbContext.Update(entity);
+        public void Update(TEntity entity) => _dbContext.Update(entity);
     }
 }
